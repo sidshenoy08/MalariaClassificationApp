b58698337385c575fecb9a344cb432fd3cb696ee
sidshenoy08 <sidshenoy2000@gmail.com>
2022-05-27 09:09:39 +0530

    Remove code to run locally

--- app.py
+++ app.py
@@ -80,9 +80,9 @@ def upload():
     return None
 
     #this section is used by gunicorn to serve the app on Heroku
-# if __name__ == '__main__':
-#         app.run()
+if __name__ == '__main__':
+        app.run()
     #uncomment this section to serve the app locally with gevent at:  http://localhost:5000
     # Serve the app with gevent 
-http_server = WSGIServer(('', 5000), app)
-http_server.serve_forever()
+# http_server = WSGIServer(('', 5000), app)
+# http_server.serve_forever()
