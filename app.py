18aeabcf69d64077477020d599750bfa00fabc10
sidshenoy08 <sidshenoy2000@gmail.com>
2022-06-02 17:53:48 +0530

    Make image preview dynamic

--- app.py
+++ app.py
@@ -6,30 +6,30 @@ import glob
 import re
 import numpy as np
 
-# Keras
+
 from keras.applications.imagenet_utils import preprocess_input, decode_predictions
 from keras.models import load_model
 from tensorflow.keras.preprocessing import image
 
-# Flask utils
+
 from flask import Flask, redirect, url_for, request, render_template
 from werkzeug.utils import secure_filename
 from gevent.pywsgi import WSGIServer
 
-# Define a flask app
+
 app = Flask(__name__)
 
-# Model saved with Keras model.save()
+
 MODEL_PATH = 'models/encoder_dense.h5'
 
-# Load your trained model
+
 model = load_model(MODEL_PATH)
-# model._make_predict_function()          # Necessary to make everything ready to run on the GPU ahead of time
+# model._make_predict_function()         
 print('Model loaded. Start serving...')
 
 
 def model_predict(img_path, model):
-    img = image.load_img(img_path, target_size=(64,64)) # target_size must agree with what the trained model expects!!
+    img = image.load_img(img_path, target_size=(64,64))
 
     # Preprocessing the image
     img = image.img_to_array(img)
@@ -43,29 +43,26 @@ def model_predict(img_path, model):
 
 @app.route('/', methods=['GET'])
 def index():
-    # Main page
     return render_template('index.html')
 
 
 @app.route('/predict', methods=['GET', 'POST'])
 def upload():
     if request.method == 'POST':
-        # Get the file from post request
         f = request.files['file']
 
-        # Save the file to ./uploads
+        
         basepath = os.path.dirname(__file__)
         file_path = os.path.join(
             basepath, 'uploads', secure_filename(f.filename))
         f.save(file_path)
 
-        # Make prediction
+        
         pred = model_predict(file_path, model)
-        os.remove(file_path)  # removes file from the server after prediction has been returned
+        os.remove(file_path) 
 
 
-        # Arrange the correct return according to the model. 
-		# In this model 1 is parasitized and 0 is normal.
+        
         str1 = 'The given blood sample is parasitized/infected. Please contact the medical authorities immediately.'
         str2 = 'The given blood sample is normal/uninfected. Stay safe!'
         if pred[0] == 1:
@@ -74,12 +71,10 @@ def upload():
             return str2
     return None
 
-    # this section is used by gunicorn to serve the app on Heroku
-if __name__ == '__main__':
-        app.run()
-    # uncomment this section to serve the app locally with gevent at:  http://localhost:5000
-    # Serve the app with gevent
+    
+# if __name__ == '__main__':
+#         app.run()
 
 
-# http_server = WSGIServer(('', 5000), app)
-# http_server.serve_forever()
+http_server = WSGIServer(('', 5000), app)
+http_server.serve_forever()
